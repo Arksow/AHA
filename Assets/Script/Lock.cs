edabc2b3761825c92ab9d0dcb04858a9154a508e
aries <creed3234@gmail.com>
2023-11-06 12:00:40 +0800

    Level 1 blocking (aries)

---
+++ Assets/Script/Lock.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class Lock : MonoBehaviour
+{
+    int firstNum;
+    int seccondNum;
+    int thirdNum;
+    int wholeNum;
+    int[] lockNum = new int[3];
+    int[] inputLockNum = new int[3];
+
+    public TMP_Text inputLockText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lockNum[0] = Random.Range(1, 9);
+        lockNum[1] = Random.Range(0, 9);
+        lockNum[2] = Random.Range(0, 9);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        firstNum = lockNum[0];
+        seccondNum = lockNum[1];
+        thirdNum = lockNum[2];
+
+        wholeNum = firstNum * 100 + seccondNum * 10 + thirdNum;
+        if (inputLockText.text == wholeNum.ToString())
+        {
+            inputLockText.text = "Win";
+        }
+    }
+
+    public void SetInputValue(string s)
+    {
+        inputLockText.text += s;
+       
+    }
+}
---
+++ Assets/Script/PlayerScript/CharacterMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    [SerializeField]private Vector3 playerVelocity;
+    [SerializeField]private bool groundedPlayer;
+    [SerializeField]float speed;
+    public float playerWalkSpeed;
+    public float playerRunSpeed;
+    public float jumpHeight;
+    public float gravityValue;
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+
+    private void Start()
+    {
+       
+
+    }
+
+    void Update()
+    {
+        groundedPlayer = Physics.CheckSphere(groundCheck.position,groundDistance,groundMask);
+
+        if (playerVelocity.y < 0 && groundedPlayer)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2 * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftShift)&&groundedPlayer)
+        {
+            speed = playerRunSpeed;
+        }
+        else
+        {
+            speed = playerWalkSpeed;
+        }
+        
+        
+        
+    }
+}
