c34d1f315d973555a6776fc7202bc9a2361b7dfa
21-Aries <creed3234@gmail.com>
2023-11-13 15:04:07 +0800

    Added jumpscare script(aries)

---
+++ Assets/Script/Jumpscare.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumpscare : MonoBehaviour
+{
+    public GameObject scareObject;
+    public AudioSource audiosource;
+    public AudioClip scare;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            scareObject.SetActive(true);
+            audiosource.PlayOneShot(scare);
+            Destroy(scareObject, 2);
+        }
+    }
+}
