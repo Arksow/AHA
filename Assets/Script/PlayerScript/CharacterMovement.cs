5520f9100f0581e8d992c5481606d075bb1514a9
03_LamHauJin <minimx1999@gmail.com>
2023-11-14 14:25:11 +0800

    Add crouch for player
    Add crouch for player by Lam hau jin

--- Assets/Script/PlayerScript/CharacterMovement.cs
+++ Assets/Script/PlayerScript/CharacterMovement.cs
@@ -7,12 +7,14 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    public Camera PlayerCam;
     public CharacterController controller;
     [SerializeField]private Vector3 playerVelocity;
     [SerializeField]private bool groundedPlayer;
     [SerializeField]float speed;
     public float playerWalkSpeed;
     public float playerRunSpeed;
+    public float playerCrouchSpeed;
     public float jumpHeight;
     public float gravityValue;
     public Transform groundCheck;
@@ -25,7 +27,7 @@ public class CharacterMovement : MonoBehaviour
 
     private void Start()
     {
-       
+        PlayerCam = GetComponentInChildren<Camera>();
 
     }
 
@@ -54,12 +56,26 @@ public class CharacterMovement : MonoBehaviour
 
         controller.Move(playerVelocity * Time.deltaTime);
 
-        if (Input.GetKey(KeyCode.LeftShift)&&groundedPlayer)
+        if (Input.GetKey(KeyCode.LeftShift)&&groundedPlayer&&!Input.GetKey(KeyCode.C))
         {
+            PlayerCam.transform.localPosition = new Vector3(0, 0.5f, 0);
             speed = playerRunSpeed;
+            controller.height = 1.8f;
+            controller.center = new Vector3(0, 0, 0);
         }
+        else if (Input.GetKey(KeyCode.C) && groundedPlayer)
+        {
+            PlayerCam.transform.localPosition = new Vector3(0, 0.25f, 0);
+            speed = playerCrouchSpeed;
+            controller.height = 1.2f;
+            controller.center = new Vector3(0, -0.3f, 0);
+        }       
         else
         {
+            PlayerCam.transform.localPosition = new Vector3(0, 0.5f, 0);
+            
+            controller.height = 1.8f;
+            controller.center = new Vector3(0, 0, 0);
             speed = playerWalkSpeed;
         }
         
